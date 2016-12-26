d4627395067ce7065b3f91cf0bd4a469d00eafbe
Zi Ning <Ningersan@outlook.com>
2016-12-26 14:54:45 +0800

    refactoring

--- Task2/Task18/scripts/Task18.js
+++ Task2/Task18/scripts/Task18.js
@@ -3,45 +3,74 @@ var btnList = document.getElementsByTagName("button");
 var numArea = document.getElementById("num-area");
 
 //初始化队列
-var queue = [];
+var queue = {
+    items: [],
+
+    leftPush: function () {
+        if (getValue()) {
+            queue.items.unshift(getValue());
+            queue.render();
+        }
+    },
+
+    rightPush: function () {
+        if (getValue()) {
+            queue.items.push(getValue());
+            queue.render();
+        }
+    },
+
+    leftPop: function () {
+        alert(queue.items.shift());
+        queue.render();
+    },
+
+    rightPop: function () {
+        alert(queue.items.pop());
+        queue.render();
+    },
+
+    deleteId: function (id) {
+        queue.items.splice(id, 1);
+        queue.render();
+    },
+
+    render: function () {
+        var item = "";
+        for (var i in queue.items) {
+            item += "<div>"+queue.items[i]+"</div>";
+        }
+        numArea.innerHTML = item;
+        divEvent();
+    }
+}
+
+function getValue() {
+    if (!isNaN(numInput.value) && numInput.value != "") {
+        return numInput.value;
+    }
+    else {
+        alert("please enter a number");
+    }
+}
 
 //给每个按钮绑定事件
-var functs = [addFromLeft, addFromRight, delFromLeft, delFromRight];
-for (var i = 0; i < 4; i++) {
-    btnList[i].addEventListener("click", functs[i]);
-}
-
-function addFromLeft() {
-    var item = "<div>"+numInput.value+"</div>";
-    var temp = queue; //临时存放队列
-    queue = [];
-    queue[0] = item;
-    for (var i in temp) {
-        queue.push(temp[i]);
+function btnEvent() {
+    var functs = [queue.leftPush, queue.rightPush, queue.leftPop, queue.rightPop];
+    for (var i = 0; i < 4; i++) {
+        btnList[i].addEventListener("click", functs[i]);
     }
-    renderQueue();
 }
 
-function addFromRight() {
-    var item = "<div>"+numInput.value+"</div>";
-    queue.push(item);
-    renderQueue();
-}
-
-function delFromLeft() {
-    queue.shift();
-    renderQueue();
-}
-
-function delFromRight() {
-    queue.pop();
-    renderQueue();
-}
-
-function renderQueue() {
-    var items = "";
-    for (var i in queue) {
-        items += queue[i];
+//给num-area中的每个div绑定事件
+function divEvent() {
+    var len = numArea.childNodes.length;
+    for (var i = 0; i < len; i++) {
+        numArea.childNodes[i].addEventListener("click", (function (i) {
+            return function(){return queue.deleteId(i)};
+        })(i));
     }
-    numArea.innerHTML = items;
 }
+
+btnEvent();
+
