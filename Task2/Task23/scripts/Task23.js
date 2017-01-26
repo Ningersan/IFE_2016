29d47d5610ea08bcf085461fd4ebfc1faadb0da9
Zi Ning <Ningersan@outlook.com>
2017-01-26 11:37:16 +0800

    update

--- Task2/Task23/scripts/Task23.js
+++ Task2/Task23/scripts/Task23.js
@@ -21,11 +21,7 @@ function renderTreeNode(nodeList) {
     //查找比较元素
     function isEqual(node) {
         var input = $("input").value.trim();
-        if (input == node.firstChild.nodeValue.trim()) {
-            return true;
-        } else {
-            return false;
-        }
+        return input == node.firstChild.nodeValue.trim();
     }
 
     setDisabled(true);
