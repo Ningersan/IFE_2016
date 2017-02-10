ca57bcb75b87dfbff4b2af4c5a9a07cdbbf83d45
Zi Ning <Ningersan@outlook.com>
2017-02-10 18:57:13 +0800

    to be continued

--- Task2/Task32/scripts/Task32.js
+++ Task2/Task32/scripts/Task32.js
@@ -5,6 +5,10 @@ function $(ele) {
     return document.querySelector(ele);
 }
 
+function $a(ele) {
+    return document.querySelectorAll(ele);
+}
+
 //联动表单
 function showOption(flag) {
     if (flag) {
@@ -108,8 +112,66 @@ function checkNum(ele) {
     }
 }
 
-function dataProduct() {
+var validator = {
+    lengthControl: function (target, data) {
+        var text = target.value;
+        var minLength = data.minLength;
+        var maxLength = data.maxLength;
+        if (getLength(text) >= minLength && getLength(text) <= maxLength) {
+            return true;
+        } else {
+            return false;
+        }
+        function getLength(str) {
+            var count = 0;
+            for (var i = 0; i < str.length; i++) {
+                var charCode = str.charCodeAt(i);
+                if (charCode >= 0 && charCode <= 128) {
+                    count += 1;
+                } else {
+                    count += 2;
+                }
+            }
+            return count;
+        }
+    },
 
+    number: function () {
+        var text = this.ipt.value;
+        if (text.match(/^\d*$/)) {
+            this.trueTip;
+            return true;
+        } else {
+            this.errorTip;
+            return false;
+        }
+    },
+
+    email: function () {
+        var text = this.ipt.value;
+        if (value.match(/^[a-zA-Z0-9_\.\-]+@[^\.@]+\.[a-z]+$/)) {
+            this.trueTip;
+            return true;
+        } else {
+            this.errorTip;
+            return false;
+        }
+    },
+
+    tel: function () {
+        var text = this.ipt.value;
+        if (value.match(/^1\d{10}$/)) {
+            this.trueTip;
+            return true;
+        } else {
+            this.errorTip;
+            return false;
+        }
+    },
+}
+
+
+function dataProduct() {
 }
 
 dataProduct.prototype = {
@@ -130,10 +192,38 @@ dataProduct.prototype = {
             inputType: "",
             items: [],
             necessary: true,
-            validator: function () {
-            }  //表单验证规则
+            minLength: "",
+            maxLength: "",
+            remainder: "",
+            successRemainder: "",
+            failRemainder: "",
+            validator: function (str, data) {     //表单验证规则
+            }
         }
         data = this.getBaseData(data);
+        switch (data.type) {
+            case "input":
+                switch (data.inputType) {
+                    case "text":
+                    case "password":
+                        data = this.getLengthRelativeData(data);
+                        break;
+                    case "number":
+                    case "email":
+                    case "tel":
+                        data = this.getContactRelativeData(data);
+                        break;
+                }
+                break;
+            case "radio":
+            case "checkbox":
+            case "pulldown":
+                data = this.getOptionRelativeData(data);
+                break;
+            case "textarea":
+                data = this.getLengthRelativeData(data);
+                break;
+        }
         return data;
     },
 
@@ -142,6 +232,32 @@ dataProduct.prototype = {
         data.type = getType(this.type);
         data.inputType = getType(this.rule);
         if (data.type != "input" && data.type != "textarea") data.items = getOptions();
+        return data;
+    },
+
+    getLengthRelativeData: function (data) {
+        data.minLength = $("#min-length").value;
+        data.maxLength = $("#max-length").value;
+        data.remainder = (data.necessary? "必填": "选填") + "，长度为" + data.minLength + "-" + data.maxLength + "个字符";
+        data.successRemainder = data.label + "格式正确";
+        data.failRemainder = data.label + "长度不能小于" + data.minLength +"个字符，" + "长度不能大于" + data.maxLength +"个字符！"
+        data.validator = validator.lengthControl;
+        return data;
+    },
+
+    getContactRelativeData: function (data) {
+        data.remainder = (data.necessary? "必填": "选填") + "，请输入您的" + data.label;
+        data.successRemainder = data.label + "格式正确！";
+        data.failRemainder = data.label + "格式不正确，请检查！";
+        data.validator = validator;
+        return data;
+    },
+
+    getOptionRelativeData: function (data) {
+        data.remainder = (data.necessary? "必填": "选填") + "，请选择您的" + data.label;
+        data.successRemainder = data.label + "单选框已选择";
+        data.failRemainder = data.label + "未选择！";
+        data.validator = validator;
         return data;
     },
 
@@ -173,6 +289,8 @@ dataProduct.prototype = {
 
         $(".rule").addEventListener("change", function (e) {
             var targetId = e.target.id;
+            //同步配置名称
+            $("#name").value = e.target.nextSibling.nextSibling.textContent;
             switch (targetId) {
                 case "text":
                     break;
@@ -210,7 +328,7 @@ dataProduct.prototype = {
 
     addInputForm: function (data) {
         var box = document.createElement("div");
-        box.innerHTML = "<label>" + data.label + "</label><input type='" + data.inputType + "'>";
+        box.innerHTML = "<label>" + data.label + "</label><input type='" + data.inputType + "'><span class='hidden'>"+ data.remainder +"</span>";
         this.resultForm.insertBefore(box, this.submitBtn);
     },
 
@@ -221,6 +339,7 @@ dataProduct.prototype = {
         for (var i = 0; i < data.items.length; i++) {
             text += "<input type='radio' name='option'><label>" + data.items[i] + "</label>";
         }
+        text += "<span class='hidden'>"+ data.remainder +"</span>";
         box.innerHTML = text;
         this.resultForm.insertBefore(box, this.submitBtn);
     },
@@ -232,6 +351,7 @@ dataProduct.prototype = {
         for (var i = 0; i < data.items.length; i++) {
             text += "<input type='checkbox' name='option'><label>" + data.items[i] + "</label>";
         }
+        text += "<span class='hidden'>"+ data.remainder +"</span>";
         box.innerHTML = text;
         this.resultForm.insertBefore(box, this.submitBtn);
     },
@@ -243,14 +363,14 @@ dataProduct.prototype = {
         for (var i = 0; i < data.items.length; i++) {
             text += "<option>" + data.items[i] + "</option>";
         }
-        text += "</select>";
+        text += "</select><span class='hidden'>"+ data.remainder +"</span>";
         box.innerHTML = text;
         this.resultForm.insertBefore(box, this.submitBtn);
     },
 
     addTextareaForm: function (data) {
         var box = document.createElement("div");
-        box.innerHTML = "<label>" + data.label + "</label><textarea></textarea>"
+        box.innerHTML = "<label>" + data.label + "</label><textarea></textarea><span class='hidden'>"+ data.remainder +"</span>"
         this.resultForm.insertBefore(box, this.submitBtn);
     }
 }
@@ -262,5 +382,27 @@ $("#btn-add").onclick = function () {
     var data = test.getData();
     test.addForm(data);
     test.submitBtn.className = "btn-sub";
+    var inputEles = $(".result-form").getElementsByTagName("input");
+    for (var i = 0; i < inputEles.length; i++) {
+        //绑定焦点事件
+        inputEles[i].addEventListener("focus", function (e) {
+            var target = e.target;
+            var tip = target.nextSibling;
+            tip.className = "remainder";
+            target.style.borderColor = "#0dc1c1";
+        })
+
+        //绑定离开焦点事件
+        inputEles[i].addEventListener("blur", function (e) {
+            var isTrue = data.validator(e.target, data);
+            if (isTrue) {
+                e.target.nextSibling.style.color = "#0dc1c1";
+            } else {
+                e.target.nextSibling.style.color = "#F04C57";
+                e.target.className = "error";
+            }
+        })
+    }
 };
+
 
