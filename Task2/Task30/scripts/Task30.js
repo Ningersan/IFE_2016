9489009fea13f8c1a86ce9e526bc970f4a741379
zining <ningersan@outlook.com>
2017-05-29 16:40:59 +0800

    refactor code

--- Task2/Task30/scripts/Task30.js
+++ Task2/Task30/scripts/Task30.js
@@ -9,109 +9,124 @@ function $a(ele) {
     return document.querySelectorAll(ele);
 }
 
-function getLength(str) {
-    var count = 0;
-    for (var i = 0; i < str.length; i++) {
-        var charCode = str.charCodeAt(i);
-        if (charCode >= 0 && charCode <= 128) {
-            count += 1;
-        } else {
-            count += 2;
-        }
-    }
-    return count;
-}
+var inputEle = $a(".sign-up input");
 
-function checkValue(target) {
-    var check;
-    var id = target.id;
-    var value = target.value.trim();
-    var tips = target.nextSibling.nextSibling;
+var tips = {
+    name: ['名称格式正确！', '请检查字符数！'],
+    password: ['密码格式正确！', '格式错误，请重新输入密码'],
+    passwordChecked: ['密码正确！', '两次输入密码不一致，请重新检查'],
+    email: ['邮箱格式正确！', '邮箱格式错误，请重新检查'],
+    tel: ['手机格式正确！', '手机格式错误，请重新检查']
+};
 
-    function render(str, isAlert) {
-        if (isAlert) {
-            tips.innerHTML = str;
-            tips.style.color = "lightgreen";
-            target.style.borderColor = "lightgreen";
-            check = true;
-        } else {
-            tips.innerHTML = str;
-            tips.style.color = "red";
-            target.style.borderColor = "red";
-            check = false;
-        }
-    }
+var validator = {
+    name: /^[a-zA-Z0-9_]{4,16}$/,
+    password: /^[a-zA-Z0-9_]{6,18}$/,
+    passwordChecked: /^[a-zA-Z0-9_]{6,18}$/,
+    email: /^[a-zA-Z0-9_\.\-]+@[^\.@]+\.[a-z]+$/,
+    tel: /^1\d{10}$/
+};
 
-    if (value === "" || value === null) {
-        render("内容不能为空", false);
-        tips.style.display = "block";
-        return;
+/**
+ * 检查输入是否合法
+ * @param {string} value --  检查的值
+ */
+function checkInputValue(element) {
+    var id = element.id;
+
+    var rightTips = tips[id][0];
+    var errorTips = tips[id][1];
+    var tipsEle = element.nextElementSibling;
+    var showRemainder = renderTips(tipsEle);
+    var value = id === 'name' ? element.value.trim().replace(/[\u0391-\uFFE5]/g, "cc") : element.value.trim();
+
+    if (!value) {
+        showRemainder("内容不能为空", false);
+        return false;
     }
 
-    if (id === "name") {
-        if (getLength(value) >= 4 && getLength(value) <= 16) {
-            render("名称格式正确！", true);
+    if (id === 'passwordChecked') {
+        if ($('#password').value.trim() === value) {
+            showRemainder(rightTips, true);
+            return true;
         } else {
-            render("请检查字符数！", false);
+            showRemainder(errorTips, false);
+            return false;
         }
-    } else if (id === "password") {
-        if (value.match(/^[a-zA-Z0-9]{6,18}$/)) {
-            render("密码格式正确！", true);
-        } else {
-            render("格式错误，请重新输入密码", false);
-        }
-    } else if (id === "password-checked") {
-        if ($a("input")[1].value.trim() === value) {
-            render("密码正确！", true);
-        } else {
-            render("两次输入密码不一致，请重新检查", false);
-        }
-    } else if (id === "email") {
-        var myrReg = /^[a-zA-Z0-9_\.\-]+@[^\.@]+\.[a-z]+$/;
-        if (value.match(myrReg)) {
-            render("邮箱格式正确！", true);
-        } else {
-            render("邮箱格式错误，请重新检查", false);
-        }
+    }
+
+    if (validator[id].test(value)) {
+        showRemainder(rightTips, true);
+        return true;
     } else {
-        if (value.match(/^1\d{10}$/)) {
-            render("手机格式正确！", true);
-        } else {
-            render("手机格式错误，请重新检查", false);
-        }
+        showRemainder(errorTips, false);
+        return false;
     }
-    return check;
 }
 
-//绑定事件区
-var inputEle = $a("input");
-for (var i = 0; i < inputEle.length; i++) {
-    //绑定焦点事件
-    inputEle[i].addEventListener("focus", function (e) {
-        var target = e.target;
-        var tips = target.nextSibling.nextSibling;
-        target.style.borderColor = "#0dc1c1";
-        tips.style.display = "block";
-        tips.style.color = "#a8a8a8";
-        tips.innerHTML = tips.parentNode.getAttribute("data-info");
-    })
+/**
+ * curry
+ * 显示提示
+ * @param {*Node} tipsEle - 提示元素
+ */
+function renderTips(tipsEle) {
+    var inputEle = tipsEle.previousElementSibling;
 
-    //绑定离开焦点事件
-    inputEle[i].addEventListener("blur", function (e) {
-        checkValue(e.target);
-    })
-}
+    return function(tips, isValid) {
+        tipsEle.innerHTML = tips;
+        tipsEle.style.display = "block";
 
-$("button").addEventListener("click", function () {
-    for (var i = 0; i < inputEle.length; i++) {
-        if (!checkValue(inputEle[i])) {
-            alert("提交失败");
+        // 默认提示
+        if (typeof isValid === 'undefined') {
+            tipsEle.className = 'tips';
+            inputEle.style.borderColor = '#75b86b';
             return;
         }
+
+        tipsEle.className = isValid ? 'success' : 'error';
+        inputEle.style.borderColor = isValid ? '#4fc08d' : '#f75659';
+    };
+}
+
+// focus, blur, load 都不触发冒泡，但可以触发捕获
+addEvent($('.sign-up'), 'focus', function(e) {
+    var inputEle = e.target;
+    var labelEle = inputEle.previousElementSibling;
+    var tipsEle = inputEle.nextElementSibling;
+    var render = renderTips(tipsEle);
+
+    // set style
+    // 默认提示
+    render(labelEle.dataset.info);
+});
+
+addEvent($('.sign-up'), 'blur', function(e) {
+    checkInputValue(e.target);
+});
+
+addEvent($('.submit'), 'click', function(e) {
+    var isAllValid = true;
+
+    inputEle.forEach(function(input) {
+        if (!checkInputValue(input)) {
+            isAllValid = false;
+        }
+    });
+
+    if (isAllValid) {
+        alert('提交成功');
+    } else {
+        alert('提交失败');
     }
-    alert("提交成功");
-})
+});
 
-
-
-
+//事件绑定函数，兼容浏览器差异（捕获）
+function addEvent(element, event, listener) {
+    if (element.addEventListener) {
+        element.addEventListener(event, listener, true);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + event, listener);
+    } else {
+        element["on" + event] = listener;
+    }
+}
